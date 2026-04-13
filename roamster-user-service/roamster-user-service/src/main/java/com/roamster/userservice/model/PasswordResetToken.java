7ba18f061275df29d69275638ac002189ca87ee5
Rajan Parmar <rajanparmar2522@gmail.com>
2026-04-13 17:38:31 +0530

    Added the passwordResetToken table

---
+++ roamster-user-service/roamster-user-service/src/main/java/com/roamster/userservice/model/PasswordResetToken.java
@@ -0,0 +1,33 @@
+package com.roamster.userservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "password_reset_tokens")
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String otp;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(nullable = false)
+    private boolean used = false;
+
+}
